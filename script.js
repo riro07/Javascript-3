2aa44da99f0f826a8f1266b3b4a44a908d36f407
Gabi <gabrielleonardomendoza19@gmail.com>
2021-08-02 17:04:50 -0300

    Comienzo de proyecto

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+console.time("time");
+
+if(2+2 == 4){
+    console.log("Funciona");
+}
+
+console.timeEnd("time");
+
+let array1 = [1,2,3,4,5,6,7,8,9,0]
+
+console.table(array1);
+
+console.log("---------------------------------------------------------")
+
+document.body.addEventListener("dblclick", ()=>console.log("Hola addeventlistener!!"));
